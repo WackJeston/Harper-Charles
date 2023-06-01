37c1ad0a78ff04d00a7669bef3832c9515882f53
Jack Weston <jack-weston99@protonmail.com>
2023-06-01 15:39:36 +0100

    Begun Pagination For Admin Tables

--- resources/js/vue/components/admin/products/productsTable.vue
+++ resources/js/vue/components/admin/products/productsTable.vue
@@ -1,29 +1,42 @@
 <template>
   <table class="web-box">
-    <tr>
-      <th id="column1">#</th>
-      <th id="column2">Title</th>
-      <th id="column3" class="hide-mobile">Subtitle</th>
-      <th id="column4">Product #</th>
-      <th id="column5"></th>
-    </tr>
+		<thead>
+			<tr>
+				<th id="column1">#</th>
+				<th id="column2">Title</th>
+				<th id="column3" class="hide-mobile">Subtitle</th>
+				<th id="column4">Product #</th>
+				<th id="column5"></th>
+			</tr>
+		</thead>
 
-    <tr v-for="(product, i) in this.products">
-      <td id="column1"><div>{{ product.id }}</div></td>
-      <td id="column2"><div>{{ product.title }}</div></td>
-      <td id="column3" class="hide-mobile"><div>{{ product.subtitle }}</div></td>
-      <td id="column4"><div>{{ product.productNumber }}</div></td>
-      <td id="column5" class="tr-buttons">
-        <a :href="'/admin/product-profile/' + product.id">
-          <i class="fa-solid fa-arrow-up-right-from-square">
-            <div class="button-label">
-              <p>Manage Product</p>
-              <div></div>
-            </div>
-          </i>
-        </a>
-      </td>
-    </tr>
+		<tbody>
+			<tr v-for="(product, i) in this.products">
+				<td id="column1"><div>{{ product.id }}</div></td>
+				<td id="column2"><div>{{ product.title }}</div></td>
+				<td id="column3" class="hide-mobile"><div>{{ product.subtitle }}</div></td>
+				<td id="column4"><div>{{ product.productNumber }}</div></td>
+				<td id="column5" class="tr-buttons">
+					<a :href="'/admin/product-profile/' + product.id">
+						<i class="fa-solid fa-arrow-up-right-from-square">
+							<div class="button-label">
+								<p>Manage Product</p>
+								<div></div>
+							</div>
+						</i>
+					</a>
+				</td>
+			</tr>
+		</tbody>
+
+		<tfoot>
+			<nav>
+				<ul class="pagination-controls">
+					<li>Previous</li>
+					<li>Next</li>
+				</ul>
+			</nav>
+		</tfoot>
 
     <div v-show="this.products == false" class="empty-table">
       <h3>No Products</h3>
