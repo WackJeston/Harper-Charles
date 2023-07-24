f556c5d1b1a68ea2663609a23b166d1523aca6cb
WackJeston <33kcaj33@gmail.com>
2023-07-24 02:08:12 +0100

    More dataForm progress

--- app/Http/Controllers/AdminTestController.php
+++ app/Http/Controllers/AdminTestController.php
@@ -12,12 +12,12 @@ class AdminTestController extends Controller
 
 		$dataForm = new dataForm(request(), '/admin/test');
 
-		$dataForm->addInput('text', 'firstName', 'First Name', null, true, 'John');
-		$dataForm->addInput('text', 'lastName', 'Last Name', null, true, 'Doe');
-		$dataForm->addInput('email', 'email', 'Email', null, true);
-		$dataForm->addInput('password', 'password', 'Password', null, true);
-		$dataForm->addInput('password', 'password_confirmation', 'Confirm Password', null, true);
-		$dataForm->addInput('checkbox', 'realPerson', 'Real Person', null, true);
+		$dataForm->addInput('text', 'firstName', 'First Name', null, 50, 1, true, 'John');
+		$dataForm->addInput('text', 'lastName', 'Last Name', null, 50, 1, true, 'Doe');
+		$dataForm->addInput('email', 'email', 'Email', null, 50, 1, true);
+		$dataForm->addInput('password', 'password', 'Password', null, 100, 1, true);
+		$dataForm->addInput('password', 'password_confirmation', 'Confirm Password', null, 100, 1, true);
+		$dataForm->addInput('checkbox', 'realPerson', 'Real Person', null, 1, 1, true);
 
 		$form = $dataForm->render();
 
--- app/dataForm.php
+++ app/dataForm.php
@@ -18,12 +18,14 @@ class DataForm
 		];
 	}
 
-	public function addInput(string $type, string $name, string $label = null, $value = null, bool $required = false, string $placeholder = null) {
+	public function addInput(string $type, string $name, string $label = null, $value = null, int $max = 1, int $min = 1, bool $required = false, string $placeholder = null) {
 		$this->form['inputs'][] = [
 			'type' => $type,
 			'name' => $name,
 			'label' => $label,
 			'value' => $value,
+			'max' => $max,
+			'min' => $min,
 			'required' => $required,
 			'placeholder' => $placeholder,
 		];
@@ -33,57 +35,66 @@ class DataForm
 		$html = sprintf('
 		<form class="web-box dk" href="%s" method="%s">', $this->form['action'], $this->form['method']);
 
-		$html .= sprintf('
-		<input type="hidden" name="_token" value="%s" />', $this->form['token']);
+			$html .= sprintf('
+			<input type="hidden" name="_token" value="%s" />', $this->form['token']);
 
-		foreach ($this->form['inputs'] as $key => $input) {
-			switch ($input['type']) {
-				case 'text':
-					$html .= sprintf('
-					<label for="%1$s">%2$s%6$s</label>
-					<input type="text" name="%1$s" value="%3$s" placeholder="%4$s" %5$s />',
-						$input['name'],
-						$input['label'],
-						$input['value'],
-						$input['placeholder'],
-						$input['required'] ? 'required' : '',
-						$input['required'] ? '<span> *</span>' : ''
-					);
-					break;
-				
-				case 'password':
-					$html .= sprintf('
-					<label for="%1$s">%2$s%6$s</label>
-					<label for="password" class="show-password">
-						<i id="show-password" class="fa-solid fa-eye"></i>
-					</label>
-					<input type="password" name="%1$s" value="%3$s" placeholder="%4$s" %5$s />',
-						$input['name'],
-						$input['label'],
-						$input['value'],
-						$input['placeholder'],
-						$input['required'] ? 'required' : '',
-						$input['required'] ? '<span> *</span>' : ''
-					);
-					break;
+			foreach ($this->form['inputs'] as $key => $input) {
+				switch ($input['type']) {
+					case 'text':
+						$html .= sprintf('
+						<label for="%1$s">%2$s%8$s</label>
+						<input type="text" name="%1$s" value="%3$s" minlength="%4$s" maxlength="%5$s" placeholder="%6$s" %7$s />',
+							$input['name'],
+							$input['label'],
+							$input['value'],
+							$input['min'],
+							$input['max'],
+							$input['placeholder'],
+							$input['required'] ? 'required' : '',
+							$input['required'] ? '<span> *</span>' : ''
+						);
+						break;
+					
+					case 'password':
+						$html .= sprintf('
+						<label for="%1$s">%2$s%8$s</label>
+						<label for="password" class="show-password">
+							<i id="show-password" class="fa-solid fa-eye"></i>
+						</label>
+						<input type="password" name="%1$s" value="%3$s" minlength="%4$s" maxlength="%5$s" placeholder="%6$s" %7$s />',
+							$input['name'],
+							$input['label'],
+							$input['value'],
+							$input['min'],
+							$input['max'],
+							$input['placeholder'],
+							$input['required'] ? 'required' : '',
+							$input['required'] ? '<span> *</span>' : ''
+						);
+						break;
 
-				case 'checkbox':
-					$html .= sprintf('
-					<div class="checkbox-container">
-						<label for="%1$s">%2$s%6$s</label>
-						<input type="checkbox" name="%1$s" value="%3$s" placeholder="%4$s" %5$s />
-					</div>',
-						$input['name'],
-						$input['label'],
-						$input['value'],
-						$input['placeholder'],
-						$input['required'] ? 'required' : '',
-						$input['required'] ? '<span> *</span>' : ''
-					);
-					break;
-				
+					case 'checkbox':
+						$html .= sprintf('
+						<div class="checkbox-container">
+							<input type="checkbox" name="%1$s" value="%3$s" minlength="%4$s" maxlength="%5$s" placeholder="%6$s" %7$s />
+							<label for="%1$s">%2$s%8$s</label>
+						</div>',
+							$input['name'],
+							$input['label'],
+							$input['value'],
+							$input['min'],
+							$input['max'],
+							$input['placeholder'],
+							$input['required'] ? 'required' : '',
+							$input['required'] ? '<span> *</span>' : ''
+						);
+						break;
+					
+				}
 			}
-		}
+
+			$html .= '
+			<button class="submit" type="submit">Save</button>';
 
 		$html .= '
 		</form>';
