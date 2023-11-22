be821951dfc4ccd0db89b943363e4b43a46add4b
WackJeston <33kcaj33@gmail.com>
2023-11-22 20:46:45 +0000

    homepage finished for now

--- resources/js/vue/components/public/homepage/productCategories.vue
+++ resources/js/vue/components/public/homepage/productCategories.vue
@@ -1,12 +1,14 @@
 <template>
 
   <div class="home-category-grid">
-    <a v-for="(category, i) in this.categories" class="grid-item" :href="'products/' + category.id">
-      <div class="background" :style="{ backgroundImage: 'url(https://hc-main.s3.eu-west-2.amazonaws.com/assets/' + category.fileName + ')' }">
-        <div class="overlay"></div>
-        <h3>{{ category.title }}</h3>
-      </div>
-    </a>
+		<div v-for="(category, i) in this.categories" class="grid-item-container">
+			<a class="grid-item" :href="'products/' + category.id">
+				<div class="background" :style="{ backgroundImage: 'url(https://hc-main.s3.eu-west-2.amazonaws.com/assets/' + category.fileName + ')' }">
+					<div class="overlay"></div>
+					<h3>{{ category.title }}</h3>
+				</div>
+			</a>
+		</div>
   </div>
 
 </template>
