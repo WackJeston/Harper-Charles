b9af100125568f597eab8482400e72f1fb07f74b
Jack Weston <jack-weston99@protonmail.com>
2024-02-21 21:05:33 +0000

    Update PublicController.php

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -2,30 +2,39 @@
 namespace App\Http\Controllers;
 
 Use DB;
+use Illuminate\Support\Facades\Auth;
 
 class PublicController extends Controller
 {
+	protected $sessionUser;
+
   public function __construct()
 	{
-		$sessionUser = auth()->user();
+		$this->middleware(function ($request, $next) {
+			$this->sessionUser= Auth::user();
+
+			return $next($request);
+	});
+
+		// dd($this->sessionUser);
 
 		$basketCount = 0;
 
-		if ($sessionUser != null) {
-			$basketCountData = DB::select('SELECT
-				o.*
-				FROM orders AS o
-				WHERE o.status = "basket" 
-				AND o.userId = 3
-				LIMIT 1'
-			);
+		// if ($sessionUser != null) {
+		// 	$basketCountData = DB::select('SELECT
+		// 		o.*
+		// 		FROM orders AS o
+		// 		WHERE o.status = "basket" 
+		// 		AND o.userId = 3
+		// 		LIMIT 1'
+		// 	);
 
-			if (!empty($basketCountData)) {
-				$basketCount = $basketCountData[0]->items;
-			}
-		}
+		// 	if (!empty($basketCountData)) {
+		// 		$basketCount = $basketCountData[0]->items;
+		// 	}
+		// }
 
-		view()->share('sessionUser', $sessionUser);
+		// view()->share('sessionUser', $sessionUser);
 		view()->share('basketCount', $basketCount);
 	}
 }
