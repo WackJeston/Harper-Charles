117be1ac339b0bd1f58e7854d958176e28259d7c
Jack Weston <83405373+WackJeston@users.noreply.github.com>
2024-08-08 21:36:16 +0100

    Create 2024_08_08_133671_modifying_banners_table.php

---
+++ database/migrations/2024_08_08_133671_modifying_banners_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+			DB::statement('
+				ALTER TABLE `banners`
+					MODIFY COLUMN `parentId` bigint(20) unsigned DEFAULT NULL;
+			');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('banners', function (Blueprint $table) {
+            //
+        });
+    }
+};
